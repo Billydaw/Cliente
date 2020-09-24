873bc0d4e962cdffdd975758e946e6bb892588af
Billydaw <57388644+Billydaw@users.noreply.github.com>
2020-09-24 09:53:00 +0200

    AE1-1

---
+++ AE1.js
@@ -0,0 +1,15 @@
+//1 Paso de script a JS
+console.log("Hola Mundo!");
+
+//1-A
+alert("Soy el primer script");
+
+//1-B
+let alertMessage = "Soy el primer script";
+
+alert (alertMessage);
+
+//1-C
+const ALERT_MESSAGE = alertMessage + " y estoy funcionando sobre " + navigator.appVersion;
+
+alert (ALERT_MESSAGE);
