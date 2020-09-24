d407e56d351f8941183bacf519f673a75f8b065e
Billydaw <57388644+Billydaw@users.noreply.github.com>
2020-09-24 20:55:20 +0200

    AE1-2

---
+++ AE1.js
@@ -0,0 +1,22 @@
+//1 Paso de script a JS
+console.log("Hola Mundo!");
+
+//1-A
+alert("Soy el primer script");
+
+//1-B
+let alertMessage = "Soy el primer script";
+
+alert (alertMessage);
+
+//1-C
+const ALERT_MESSAGE = alertMessage + " y estoy funcionando sobre " + navigator.appVersion;
+
+alert (ALERT_MESSAGE);
+
+//2-A
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(meses);
+
+//2-B
+console.table(meses);
